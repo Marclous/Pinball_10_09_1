d83a5ce938db9f351b946591fa0df94f57ffdf82
Marclous <47270710+Marclous@users.noreply.github.com>
2024-10-08 15:29:25 -0400

    Launcher done

---
+++ Assets/Scripts/AnimateController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class AnimateController : MonoBehaviour
+{
+    public Sprite[] spriteSet;
+    public float fps;
+
+    public GameObject treeObject;
+    private float turnSpeed = 30f;
+
+    private void FixedUpdate()
+    {
+        if (treeObject != null)
+        {
+            treeObject.transform.Rotate(new Vector3(0f, 0f, turnSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
---
+++ Assets/Scripts/LaunchListener.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LaunchListener : MonoBehaviour
+{
+    private Launcher launcherScript;
+
+    void Start()
+    {
+        GameObject launcherObj = GameObject.Find("Plunger-springjoint");
+        launcherScript = launcherObj.GetComponent<Launcher>();
+    }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.name == "ball")
+        {
+            launcherScript.isActive = false;
+        }
+    }
+}
